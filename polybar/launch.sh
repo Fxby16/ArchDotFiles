65b947cdd8a07539718a9f0547e7e4276285cbe0
Fxby16 <fabioscp0602@gmail.com>
2026-01-13 20:55:35 +0100

    Run one polybar instance for each monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
-# Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar MyBar &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload MyBar &
+  done
+else
+  polybar --reload MyBar &
+fi
